c60804cce93ea7c68f2fb4ca69e1b3315f0b7e7b
Rajaram <rajarams7200@gmail.com>
2026-06-04 07:32:56 +0000

    test

--- custom_app/custom_app/doctype/login/login.js
+++ custom_app/custom_app/doctype/login/login.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Login', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frappe.msgprint("Hello world");
+	}
 });
